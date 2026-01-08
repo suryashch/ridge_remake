852927d7b671d29afad2ff214295303765f5c8c0
Suryash Chakravarty <suryash.c@gmail.com>
2026-01-07 16:05:24 -0800

    added basic skeleton of the function

---
+++ ridge/src/ridge_remake/ridge_remake.py
@@ -0,0 +1,27 @@
+"""
+Basic recreating of the functionality of the Ridge function
+"""
+
+def get_reg_line():
+    """
+    Docstring for get_reg_line
+    """
+    pass
+
+def ridge_scatter():
+    """
+    Docstring for ridge_scatter
+    """
+    pass
+
+def ridge_scatter_line():
+    """
+    Docstring for ridge_scatter_line
+    """
+    pass
+
+def ridge_r2():
+    """
+    Docstring for ridge_r2
+    """
+    pass
